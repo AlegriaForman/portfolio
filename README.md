1e56870e42918c6cd0bed390a55246e3556d49a7
AlegriaForman <alegriaforman@gmail.com>
2016-02-02 22:13:24 -0800

    update portfolio for class-02

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portfolio
+class-02. Added more jQuery methods, changed the background color, etc.
--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,15 @@
+$('#skills').hide();
+
+$('#my-skills').on ('click', function() {
+$('#skills').slideToggle();
+});
+
+$('#contact').hide();
+
+$('#my-contact').on ('click', function() {
+$('#contact').slideToggle();
+});
+
 $('li').hover(function() {
   $(this).fadeOut(100);
   $(this).fadeIn(300);
