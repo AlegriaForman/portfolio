5c2442d6ae4130f412a9513aee9e0ff8e1b3f075
AlegriaForman <alegriaforman@gmail.com>
2016-02-12 20:15:22 -0800

    correct issues for Class-10 assignment

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,43 +1,43 @@
 (function(module){
-contentView = {};
+  contentView = {};
 
-contentView.initPortfolio = function(){
-  Pics.skillsPicture.forEach(function(a){
-      $('#skills-div').append(a.skillsPics());
+  contentView.initPortfolio = function(){
+    Pics.skillsPicture.forEach(function(a){
+        $('#skills-div').append(a.skillsPics());
+    });
+  };
+
+  $('#about-div').hide();
+  $('#skills-div').hide();
+  $('#contact-div').hide();
+  $('#about').hide();
+  $('#skills').hide();
+  $('#contact').hide();
+  
+  $('#my-about').on('click', function() {
+    $('#skills').hide();
+    $('#contact').hide();
+    $('#about').fadeIn();
+    $('#skills-div').hide();
+    $('#contact-div').hide();
+    $('#about-div').fadeIn();
   });
-};
 
-$('#about-div').hide();
-$('#skills-div').hide();
-$('#contact-div').hide();
+  $('#my-skills').on('click', function() {
+    $('#about').hide();
+    $('#contact').hide();
+    $('#skills').fadeIn();
+    $('#about-div').hide();
+    $('#contact-div').hide();
+    $('#skills-div').fadeIn();
+  });
 
-$('#about').hide();
-$('#my-about').on('click', function() {
-  $('#skills').hide();
-  $('#contact').hide();
-  $('#about').fadeIn();
-  $('#skills-div').hide();
-  $('#contact-div').hide();
-  $('#about-div').fadeIn();
-});
-
-$('#skills').hide();
-$('#my-skills').on('click', function() {
-  $('#about').hide();
-  $('#contact').hide();
-  $('#skills').fadeIn();
-  $('#about-div').hide();
-  $('#contact-div').hide();
-  $('#skills-div').fadeIn();
-});
-
-$('#contact').hide();
-$('#my-contact').on('click', function() {
-  $('#about').hide();
-  $('#skills').hide();
-  $('#contact').fadeIn();
-  $('#about-div').hide();
-  $('#skills-div').hide();
-  $('#contact-div').fadeIn();
-});
+  $('#my-contact').on('click', function() {
+    $('#about').hide();
+    $('#skills').hide();
+    $('#contact').fadeIn();
+    $('#about-div').hide();
+    $('#skills-div').hide();
+    $('#contact-div').fadeIn();
+  });
 })(window);
